2d10b5fa5f33d2ad60af21775e1c8ae12273432b
Luke Francl <look@recursion.org>
2013-04-21 17:33:49 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FrontendTestingForSkeptics::Application.config.secret_token = '0a48c1b9488d627b3f529ef472a9b308f2af6bb037d2e25bc5f4021a1d52a54f86a4d57aaca2a408d62897120ac5529db12cfeefeee5645cc95482e7648d0f95'
