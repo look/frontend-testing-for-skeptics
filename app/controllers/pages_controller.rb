6d8ac5cb46e81643a2d90a7df43671c4a618bc13
Luke Francl <lfrancl@swiftype.com>
2013-07-14 19:49:49 -0700

    break XSS spec

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def xss
-    @title = 'Hello World! <script>window.xss = true;</script>'
+    @title = 'Hello World! <script>window.xss = true;</script>'.html_safe
   end
 end
