694f7eea598af30117bc9b6e82772b179068b83b
Luke Francl <lfrancl@swiftype.com>
2013-07-14 19:53:18 -0700

    break factorial spec

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,7 +18,7 @@
 // function for testing with JavaScript unit tests (see spec/javascripts/)
 function factorial(n) {
   if (n <= 1) {
-    return 1;
+    return 0;
   } else {
     return n * factorial(n-1);
   }
